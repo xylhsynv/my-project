0b76dbdde3baaaaccb816343cfe6cbe0092292d0
Khayal <xylhsynv17@gmail.com>
2024-01-19 17:45:16 +0400

    yes okey

---
+++ index.js
@@ -0,0 +1,27 @@
+const studentProfile = {
+    name:'xeyal',
+    age:18,
+    course:'bbs',
+    hobbies:['xeyal',18,'bbs']
+
+}
+delete studentProfile['course']
+const{name:studentName,age}= studentProfile
+const { name,...rest}=studentProfile
+//console.log(rest);
+
+const grades=[85,92,78,95]
+
+console.log(grades[0]);
+console.log(grades[1]);
+
+const additionalInfo={
+    adderss:'bileceri',
+    phoneNuber:'775037461',
+}
+
+
+const completeProfil={
+    ...studentProfile,...additionalInfo
+}
+console.log(completeProfil);
---
+++ xeyal.js
@@ -0,0 +1,16 @@
+const personalProfil={
+    fristName: "Xeyal",
+    lastName:"Hüseynov",
+    age:17,
+    favoriteLonguge: "javaScript",
+
+    
+    contactInfo:{
+        email:'xylhsynv17@gmail.com',
+        phone:775037461,
+        contactInfo: 'bilceridoma',
+    }
+   
+}
+
+
