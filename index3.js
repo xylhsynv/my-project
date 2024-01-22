d78b614824d7ab0a3f2ad6893bf84e3b81e7b72a
Khayal <xylhsynv17@gmail.com>
2024-01-22 18:36:42 +0400

    task 3

--- index3.js
+++ index3.js
@@ -1,7 +1,7 @@
 const numbers = [10, 20, 30, 40, 50]
 const [firstNumber, secondNumber] = numbers
 
-console.log(secondNumber);
+console.log(numbers);
 console.log(numbers.length);
 
 const myString = 'xeyal'
