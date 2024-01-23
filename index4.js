07e2c791dfce17295b68210ab8644ce255cbc51e
Khayal <xylhsynv17@gmail.com>
2024-01-23 12:01:49 +0400

    task4

--- index4.js
+++ index4.js
@@ -1,2 +1,33 @@
 const scores = [5, 6, 7, 8, 9]
 const [firstScore, secondScore] = scores
+console.log(scores);
+
+const student = {
+    name: 'xeyal',
+    age: 17,
+    isEnrolled: true
+}
+const { name, age } = student
+const { ...reest } = student
+console.log(student);
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isEnrolled);
+console.log(typeof firstScore);
+
+console.log(scores.length);
+
+
+const sum = firstScore + secondScore
+const difference = firstScore - secondScore
+
+console.log(">", firstScore > secondScore);
+console.log("<", firstScore < secondScore);
+
+const Result = firstScore > 5 && secondScore < 10 && student.age >= 18;
+console.log(Result);
+const result = firstScore === 6 || secondScore !== 8 || student.age < 21;
+console.log(result);
+const isEnrolledResult = !student.isEnrolled;
+console.log(isEnrolledResult);
