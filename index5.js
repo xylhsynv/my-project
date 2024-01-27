2d5f89bebf28047527f22cd7eda1e0816c26f755
Khayal <xylhsynv17@gmail.com>
2024-01-27 15:21:18 +0400

    task forLoop.js

---
+++ index5.js
@@ -0,0 +1,101 @@
+// const firstName = "xeyal";
+// // const age = 18;
+// const stuDent = true;
+// const school = 'uni'
+// if (firstName === 'xeyal') {
+//     console.log('bunədə metrounun içinde');
+// }
+// if (age === 18) {
+//     console.log('adam  olmadın bu yaşdada ');
+// }
+// if (stuDent) {
+//     console.log(busanda);
+// } else {
+//     console.log(o döyüslenmiş);
+// }
+
+
+
+
+// if (age == 18) {
+//     if (school == uni) {
+//         console.log('prava ala');
+//     } else {
+//         console.log('savadın çatmır');
+//     }
+// } else {
+//     console.log('prava ala bilmesen');
+// }
+
+// if (age >= 0 && age <= 19) {
+//     console.log('xeayal doşandı');
+// } else if (age >= 15 && age <= 20) {
+//     console.log("yo xeyal yaxşı imiş");
+// } else {
+//     console.log('xəyal böyübmüş');
+// }
+// undefined
+
+
+// let id = '0711';
+// if (typeof id !== 'undefined') {
+//     console.log('id' + id);
+// } else {
+//     console.log('nod id');
+// }
+
+//TERNARY OPERATOR
+//   1-ci
+
+// let age = 19;
+// let message;
+// age >= 16 ? (message = 'you can dirve') : (message = 'you cannot dirve');
+// console.log(message);
+
+// //2-ci
+// message = age >= 16 ? 'you can dirve' : 'you cannot drive';
+// console.log(message);
+
+
+// for (let i = 0; i <= Array.lenght - 1; i++) {
+//     console.log('i->', arr[i].name);
+// }
+
+// let sum = 0
+// let x = 5
+// for (let i = 0; i < x; i++) {
+//     console.log('i:', i, 'sum:', sum);
+//     sum = sum + 1
+
+// const n = 50
+// for (let i = 1; i <= n; i++) {
+//     console.log('heyat adil.');
+
+
+// }
+// // }
+
+
+
+
+// let sum = 1;
+// const n = 100
+// for (let i = 1; i <= n; i++) {
+//     sum += 1;
+// }
+// console.log("sum", sum);
+
+
+let a = 2;
+switch (a) {
+    case 1:
+        a = "one";
+        break;
+    case 2:
+        a = 'two';
+        break;
+    default:
+        a = "not found"
+        break;
+}
+console.log('naxuyy');
---
+++ index6.js
@@ -0,0 +1,37 @@
+const color = "red"
+switch (color) {
+    case 'red':
+        console.log("stop");
+        break;
+    case "green ":
+        console.log("go");
+        break;
+    case "yellow ":
+        console.log("caution");
+        break;
+    default:
+        console.log("unknown colors");
+        break;
+}
+const matrix = [[1, 2], [3, 4]];
+for (let i = 0; i < matrix.length; i++) {
+    console.log(matrix);
+
+}
+const product = {
+    name: "xeyal",
+    price: 18,
+    inStock: true
+}
+// for (const key in object) {
+//     if (Object.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
+//     }
+// }
+if (productPrice < 50 && productinStocke) {
+    console.log(finsh);
+} else {
+    console.log(nofinsh);
+}
+
+
